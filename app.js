2cfb9e7c9dfe50c8eaed10df84dd759fa4fec9fb
moezzine <ezinemoez@gmail.com>
2023-12-25 12:05:54 +0100

    feat:login user:login

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express =require('express');
 const mongoose =require("mongoose");
 const dotenv =require('dotenv');
+const cors=require("cors")
 const app = express();
 const categorieRouter=require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
 const articleRouter =require("./routes/article.route")
+const userRouter =require("./routes/user.route")
 dotenv.config()
+app.use(cors())
 app.use(express.json());
 // Connexion à la base données
 mongoose.connect(process.env.DATABASECLOUD,{
@@ -24,6 +27,7 @@ res.send("Bibliothèque");
 app.use("/api/categories",categorieRouter)
 app.use('/api/scategories', scategorieRouter);
 app.use('/api/articles', articleRouter);
+app.use('/api/users', userRouter);
 app.listen(process.env.PORT, () => {
 console.log(`Server is listening on port ${process.env.PORT}`); });
 module.exports = app;
